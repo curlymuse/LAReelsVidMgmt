0d8fefb39ff60bb6cc5ef15284ee2bf60cd28bf7
Robin Arenson <robin@unreasonableadventures.com>
2015-06-26 13:02:19 -0700

    Feed reconfigured so it pulls right from Amazon.

--- app/config/lrvm.php
+++ app/config/lrvm.php
@@ -4,4 +4,5 @@ return [
     'api_key'       => 'sn983hfshdkj3ijd',
     'podcast_dir'   => app_path() . '/storage/podcasts',
     's3_podcast_bucket' => 'lareels-podcasts',
+    's3_gateway' => 'https://s3-us-west-1.amazonaws.com',
 ];
--- app/lib/LRVM/Domain/Podcast/Podcast.php
+++ app/lib/LRVM/Domain/Podcast/Podcast.php
@@ -21,6 +21,7 @@ class Podcast extends Eloquent {
 
     public function getLinkToFile() {
 
+        return sprintf('%s/%s/%s', Config::get('lrvm.s3_gateway'), Config::get('lrvm.s3_podcast_bucket'), $this->filename);
         return URL::route('podcasts.show', $this->id);
 
     }
@@ -33,6 +34,7 @@ class Podcast extends Eloquent {
 
     public function getLength() {
 
+        return 1333334;
         $file = Config::get('lrvm.podcast_dir') . '/' .$this->filename;
         return filesize($file);
 
